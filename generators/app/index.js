d239dd20a88993f00de6efb0dbb78bd039816167
afeiship <1290657123@qq.com>
2021-01-30 19:28:30 +0800

    feat: new yo class

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,58 +1,50 @@
-"use strict";
-var yeoman = require("yeoman-generator");
-var chalk = require("chalk");
-var yosay = require("yosay");
-var yoHelper = require("yeoman-generator-helper");
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const yoHelper = require('yeoman-generator-helper');
 
-require("@jswork/next-registry-choices");
+require('@jswork/next-registry-choices');
 
-module.exports = yeoman.Base.extend({
-  prompting: function () {
+module.exports = class extends Generator {
+  prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(
-        "Welcome to the striking " +
-          chalk.red("generator-slate-plugin") +
-          " generator!"
-      )
+      yosay('Welcome to the striking ' + chalk.red('generator-slate-plugin') + ' generator!')
     );
 
-    var prompts = [
+    const prompts = [
       {
-        type: "scope",
-        name: "scope",
-        message: "Your scope (eg: @babel )?",
-        default: "jswork",
+        type: 'scope',
+        name: 'scope',
+        message: 'Your scope (eg: @babel )?',
+        default: 'jswork'
       },
       {
-        type: "list",
-        name: "registry",
-        message: "Your registry",
-        choices: nx.RegistryChoices.gets(),
+        type: 'list',
+        name: 'registry',
+        message: 'Your registry',
+        choices: nx.RegistryChoices.gets()
       },
       {
-        type: "input",
-        name: "project_name",
-        message: "Your project_name?",
-        default: yoHelper.discoverRoot,
+        type: 'input',
+        name: 'project_name',
+        message: 'Your project_name?',
+        default: yoHelper.discoverRoot
       },
       {
-        type: "input",
-        name: "description",
-        message: "Your project description?",
-      },
+        type: 'input',
+        name: 'description',
+        message: 'Your project description?'
+      }
     ];
 
     return this.prompt(prompts).then((props) => {
       this.props = props;
     });
-  },
-
-  writing: function () {
-    this.fs.copyTpl(
-      this.templatePath("{.*,*,src/*}"),
-      this.destinationPath("."),
-      this.props
-    );
   }
-});
+
+  writing() {
+    this.fs.copyTpl(this.templatePath('{.*,*,src/*}'), this.destinationPath('.'), this.props);
+  }
+};
