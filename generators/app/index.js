f8e9deaeddde0c9550682515f6ffef64ecd3837d
afeiship <1290657123@qq.com>
2021-02-01 21:55:36 +0800

    refactor: update promit

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,43 +2,13 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
-const yoHelper = require('yeoman-generator-helper');
+const prompts = require('./prompts');
 
-require('@jswork/next-registry-choices');
-
-module.exports = class extends Generator {
+module.exports = class extends (
+  Generator
+) {
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay('Welcome to the striking ' + chalk.red('generator-slate-plugin') + ' generator!')
-    );
-
-    const prompts = [
-      {
-        type: 'scope',
-        name: 'scope',
-        message: 'Your scope (eg: @babel )?',
-        default: 'jswork'
-      },
-      {
-        type: 'list',
-        name: 'registry',
-        message: 'Your registry',
-        choices: nx.RegistryChoices.gets()
-      },
-      {
-        type: 'input',
-        name: 'project_name',
-        message: 'Your project_name?',
-        default: yoHelper.discoverRoot
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Your project description?'
-      }
-    ];
-
+    this.log(yosay(`Welcome to the striking ${chalk.red('generator-slate-plugin')} generator!`));
     return this.prompt(prompts).then((props) => {
       this.props = props;
     });
---
+++ generators/app/prompts.js
@@ -0,0 +1,30 @@
+const yoHelper = require('yeoman-generator-helper');
+
+require('@jswork/next-registry-choices');
+
+
+module.exports = [
+  {
+    type: 'scope',
+    name: 'scope',
+    message: 'Your scope (eg: @babel )?',
+    default: 'jswork'
+  },
+  {
+    type: 'list',
+    name: 'registry',
+    message: 'Your registry',
+    choices: nx.RegistryChoices.gets()
+  },
+  {
+    type: 'input',
+    name: 'project_name',
+    message: 'Your project_name?',
+    default: yoHelper.discoverRoot
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Your project description?'
+  }
+];
